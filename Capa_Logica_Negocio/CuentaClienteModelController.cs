53de468fcbc9d2593d5f6de7db6ecab16f847cfe
Derek Palacios <derek2001corez@gmail.com>
2021-10-24 19:32:53 -0600

    Cracion de controlador de pagos y prreparacion de metodos

--- Capa_Logica_Negocio/CuentaClienteModelController.cs
+++ Capa_Logica_Negocio/CuentaClienteModelController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Capa_Logica_Negocio.Models;
 using Capa_Logica_Negocio.Context;
+
 namespace Capa_Logica_Negocio
 {
     public class CuentaClienteModelController
@@ -13,13 +14,6 @@ namespace Capa_Logica_Negocio
         public CuentaClienteModelController(ClinicaDBContext context) => _context = context;
 
         public IEnumerable<CuentaClienteModel> GetCuentaList() => _context.tbl_Cuenta_Cliente;
-
-
-
-
-
-
-
 
 
     }
