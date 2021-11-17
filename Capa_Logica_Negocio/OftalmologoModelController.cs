0cfd6346de8fb917e27cebfe1dd5f4b506e03623
MARIO CESAR VADO MARTÍNEZ <mario.vado1790@estu.unan.edu.ni>
2021-11-16 23:17:19 -0600

    Funcionalidad crud OMC

--- Capa_Logica_Negocio/OftalmologoModelController.cs
+++ Capa_Logica_Negocio/OftalmologoModelController.cs
@@ -7,6 +7,72 @@ namespace Capa_Logica_Negocio
 {
     public class OftalmologoModelController
     {
+        private ClinicaDBContext _context;
 
+        public OftalmologoModelController(ClinicaDBContext context) => _context = context;
+
+        public IEnumerable<OftalmologoModel> GetOftalmologoList() => _context.tbl_Oftalmologo;
+        public bool AddOftalmologo(OftalmologoModel Oftalmologo)
+        {
+            try
+            {
+                _context.tbl_Oftalmologo.Add(Oftalmologo);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public OftalmologoModel GetOftalmologoById(int IdOftalmologo)
+        {
+            try
+            {
+                var Oftalmologo = _context.tbl_Oftalmologo.Find(IdOftalmologo);
+                if (Oftalmologo == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return Oftalmologo;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool EditOftalmologo(OftalmologoModel Oftalmologo)
+        {
+            try
+            {
+                _context.tbl_Oftalmologo.Update(Oftalmologo);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public bool DeleteOftalmologo(OftalmologoModel Oftalmologo)
+        {
+            try
+            {
+                _context.tbl_Oftalmologo.Remove(Oftalmologo);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
