d41568b180a2038a4f2c7a754cf6b5e5e0bb927a
MARIO CESAR VADO MARTÍNEZ <mario.vado1790@estu.unan.edu.ni>
2021-11-16 23:16:17 -0600

    creacion clase OMC

---
+++ Capa_Logica_Negocio/OftalmologoModelController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Capa_Logica_Negocio.Models;
+using Capa_Logica_Negocio.Context;
+
+namespace Capa_Logica_Negocio
+{
+    public class OftalmologoModelController
+    {
+
+    }
+}
