5054f9888c3cac33850de5bf29debef3ddc7d4c1
MARIO CESAR VADO MARTÍNEZ <mario.vado1790@estu.unan.edu.ni>
2021-11-16 23:21:47 -0600

    creacion oftalmologo controller

---
+++ ClinicaOft_V2_UI/Controllers/OftalmologoMainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capa_Logica_Negocio;
+using Capa_Logica_Negocio.Context;
+using Capa_Logica_Negocio.Models;
+
+namespace ClinicaOft_V2_UI.Controllers
+{
+    public class OftalmologoMainController : Controller
+    {
+        
+    }
+}
