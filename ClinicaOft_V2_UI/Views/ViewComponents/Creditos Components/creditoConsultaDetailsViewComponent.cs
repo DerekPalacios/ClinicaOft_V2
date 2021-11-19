ddf29d9ef5c9e3e20db9438154c1d5fac65f3c75
Derek Palacios <derek2001corez@gmail.com>
2021-11-18 18:08:02 -0600

    anexo del controlador para vista de los detalles de la lista de creditos de consultas

---
+++ ClinicaOft_V2_UI/Views/ViewComponents/Creditos Components/creditoConsultaDetailsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capa_Logica_Negocio;
+using Capa_Logica_Negocio.Context;
+namespace ClinicaOft_V2_UI.Views.ViewComponents.Creditos_Components
+{
+    public class creditoConsultaDetailsViewComponent : ViewComponent
+    {
+        private CreditoConsultaModelController creditoConsulta;
+
+        public creditoConsultaDetailsViewComponent(ClinicaDBContext context) => creditoConsulta = new CreditoConsultaModelController(context);
+
+        public IViewComponentResult Invoke(int? idCuenta)
+        {
+            return View(creditoConsulta.GetCreditoConsultaDetailList(idCuenta));
+        }
+
+
+    }
+}
